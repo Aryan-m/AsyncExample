cef0121b9caabc362953041178b69d4e1d20806b
Aryan-m <aryan_m96@yahoo.com>
2021-11-23 17:12:08 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExample
+{
+    class Program
+    {
+        static void countTo3()
+        {
+            Task.Delay(1000);
+        }
+        static Task countTo3Async()
+        {
+            return new Task(() => Task.Delay(1000));
+        }
+
+        static async Task execAsync(Stopwatch timer)
+        {
+            timer.Start();
+            await Task.WhenAll(new List<Task>
+            {
+                  countTo3Async()
+                , countTo3Async()
+                , countTo3Async()
+            });
+            timer.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            // set up timer
+            var timer = new Stopwatch();
+
+            // first do sync
+            timer.Start();
+            countTo3();
+            countTo3();
+            countTo3();
+            timer.Stop();
+            Console.WriteLine($"Sync: {timer.ElapsedMilliseconds}");
+
+            // async
+            timer.Reset();
+            execAsync(timer);
+            Console.WriteLine($"Async: {timer.ElapsedMilliseconds}");
+
+        }
+    }
+}
